41eedbd43ed874b208417710301c15b3190b7076
srikamal237 <srikamal661@gmail.com>
2024-08-31 01:12:11 +0530

    adding insertion file

---
+++ Array/Sorting/insertionSort.java
@@ -0,0 +1,42 @@
+package Array.Sorting;
+
+import java.util.Scanner;
+
+public class insertionSort {
+    
+    public static void insertion(int[] arr,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j])
+            {
+                int temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]=temp;
+                j--;
+            }
+        }
+
+    }
+
+     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = 7;
+        int arr[] = { 9, 4, 7, 6, 3, 1, 5 };
+        System.out.println("Before sorting array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        insertion(arr,n);
+
+        System.out.println("After sorting array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+}
