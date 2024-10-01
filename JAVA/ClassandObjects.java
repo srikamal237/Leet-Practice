7b364e7ef0e7fb2eaff31ec15649ef2380bf5b0e
srikamal237 <srikamal661@gmail.com>
2024-10-01 20:28:26 +0530

    creating file

--- JAVA/ClassandObjects.java
+++ JAVA/ClassandObjects.java
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-package JAVA;
-
-public class ClassandObjects {
-
-     static String name;
-     static int roll_no;
-     static int salary;
-     ClassandObjects(String name, int roll_no, int salary)
-     {
-        this.name=name;
-        this.roll_no=roll_no;
-        this.salary=salary;
-     }
-    public static void main(String[] args){
-
-        ClassandObjects claz=new ClassandObjects("kamal",23,4);
-       
-        System.out.println(claz.name);
-        System.out.println(claz.roll_no);
-        System.out.println(claz.salary);
-
-        ClassandObjects claz2=new ClassandObjects("jega",17,40000);
-
-        System.out.println(claz2.name);
-        System.out.println(claz2.roll_no);
-        System.out.println(claz2.salary);
-
-
-    }
-}
-=======
 package JAVA;
 
 public class ClassandObjects {
@@ -51,16 +19,10 @@ public class ClassandObjects {
         ClassandObjects claz2=new ClassandObjects("jega",17,40000);
         ClassandObjects claz3=new ClassandObjects("amma",25,50000);
        
+        System.err.println(" Printing ");
         System.out.println(claz.name+ " "+claz.roll_no+ " "+claz.salary);
         System.out.println(claz2.name+ " "+claz2.roll_no+ " "+claz2.salary);
         System.out.println(claz3.name+ " "+claz3.roll_no+ " "+claz3.salary);
 
-
-
-        
-
-
-
     }
 }
->>>>>>> b21bc93 (update)
